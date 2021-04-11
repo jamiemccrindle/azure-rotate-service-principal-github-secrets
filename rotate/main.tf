64b05c8cb15546f664f505f70e823730b396a5f2
jamie mccrindle <jamiemccrindle@gmail.com>
2021-04-11 12:13:49 +0000

    slowly but surely

--- rotate/main.tf
+++ rotate/main.tf
@@ -1,22 +1,48 @@
+locals {
+  date        = tonumber(var.date)
+  odd_keeper  = floor((local.date + 1) / 2)
+  even_keeper = floor(local.date / 2)
+  use_even    = local.date % 2 == 0
+}
+
 resource "azuread_application" "example" {
-  display_name               = "example"
+  display_name = "example"
 }
 
 resource "azuread_application_password" "odd" {
   application_object_id = azuread_application.example.id
   description           = "odd"
   value                 = random_password.odd.result
-  end_date_relative     = "60d"
+  end_date_relative     = "1440h"
 }
 
 resource "random_password" "odd" {
+  keepers = {
+    "date" = local.odd_keeper
+  }
+  length           = 32
+  special          = true
+  override_special = "_%@"
+}
+
+resource "azuread_application_password" "even" {
+  application_object_id = azuread_application.example.id
+  description           = "even"
+  value                 = random_password.odd.result
+  end_date_relative     = "1440h"
+}
+
+resource "random_password" "even" {
+  keepers = {
+    "date" = local.even_keeper
+  }
   length           = 32
   special          = true
   override_special = "_%@"
 }
 
 resource "github_actions_secret" "example" {
-  repository       = "azure-rotate-service-principal-github-secrets"
-  secret_name      = "AZURE_SERVICE_PRINCIPAL"
-  plaintext_value  = random_password.odd.result
+  repository      = "azure-rotate-service-principal-github-secrets"
+  secret_name     = "AZURE_SERVICE_PRINCIPAL"
+  plaintext_value = local.use_even ? random_password.even.result : random_password.odd.result
 }
--- rotate/variables.tf
+++ rotate/variables.tf
@@ -1,3 +1,8 @@
 variable "date" {
-  description = "The date"
+  description = "The date to use for rotation should be of the form YYYYDD"
+  type = string
+  validation {
+    condition = var.date == tostring(tonumber(var.date)) && tonumber(var.date) > 0
+    error_message = "The date variable must be a positive number."
+  }
 }
